b23e07b5182f594f946de04f264fed2bb5a5a6d2
pc <pavel.kanavalau@gmail.com>
2024-04-07 20:35:20 +0300

    pluggable table with sortable and filterable plugins

--- components/Table.js
+++ components/Table.js
@@ -1,29 +1,34 @@
 class Table {
-    constructor(container, options = {}) {
-        this.container = container; // DOM элемент, в который будет рендериться таблица
-        this.options = options;
-        this.columns = []; // Массив для хранения столбцов
-        this.data = []; // Данные для отображения в таблице
-    }
+	#plugins = [];
+	#settings = {};
+	columns = [];
+	#container = null;
+	data = [];
 
-    addColumn(column) {
-		column.element = null; // Инициализируем здесь, добавим элемент позже
-        this.columns.push(column);
-    }
+	constructor(container, settings = {}) {
+		this.#container = container; // DOM элемент, в который будет рендериться таблица
+		Object.assign(this.#settings, settings);
+	}
 
-    setData(data) {
-        this.data = data;
-    }
+	addColumn(column) {
+		this.columns.push(column);
+	}
+
+	setData(data) {
+		this.data = data;
+		this.render();
+	}
 
 	addPlugin(plugin) {
-        plugin.apply(this);
-    }
+		plugin.init();
+		this.#plugins.push(plugin);
+	}
 
-    render() {
-        const tableElement = document.createElement('table');
-        tableElement.className = 'custom-table'; // Пример класса для стилизации
+	render() {
+		const tableElement = document.createElement('table');
+		tableElement.className = 'custom-table'; // Пример класса для стилизации
 
-       // Создание и добавление заголовков таблицы
+		// Создание и добавление заголовков таблицы
 		const thead = document.createElement('thead');
 		const headerRow = document.createElement('tr');
 		this.columns.forEach(column => {
@@ -35,23 +40,26 @@ class Table {
 		thead.appendChild(headerRow);
 		tableElement.appendChild(thead);
 
-        // Создание и добавление строк данных
-        const tbody = document.createElement('tbody');
-        this.data.forEach(rowData => {
-            const row = document.createElement('tr');
-            this.columns.forEach(column => {
-                const cell = document.createElement('td');
-                cell.textContent = rowData[column.field];
-                row.appendChild(cell);
-            });
-            tbody.appendChild(row);
-        });
-        tableElement.appendChild(tbody);
-        // Очистка контейнера и добавление сгенерированной таблицы
-        this.container.innerHTML = '';
-        this.container.appendChild(tableElement);
-    }
+		// Создание и добавление строк данных
+		const tbody = document.createElement('tbody');
+		this.data.forEach(rowData => {
+			const row = document.createElement('tr');
+			this.columns.forEach(column => {
+				const cell = document.createElement('td');
+				cell.textContent = rowData[column.title];
+				row.appendChild(cell);
+			});
+			tbody.appendChild(row);
+		});
+		tableElement.appendChild(tbody);
+
+		// Очистка контейнера и добавление сгенерированной таблицы
+		this.#container.innerHTML = '';
+		this.#container.appendChild(tableElement);
+
+		// Пересоздание слушателей событий плагинов
+		this.#plugins.forEach(plugin => plugin.init());
+	}
 }
 
-// Экспорт класса для использования в других частях приложения
 export default Table;
--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 import './styles/style.css';
+import FilterableTablePlugin from './components/plugins/FilterableTablePlugin.js';
+import Input from './components/Input.js';
+import SortableTablePlugin from './components/plugins/SortableTablePlugin.js';
 import Table from './components/Table.js';
-import Sortable from './components/Sortable.js';
-import Filterable from './components/Filterable.js';
 import fetchJson from './helpers/fetchData.js';
 import mergeData from './helpers/mergeData.js';
 
-const CARS_DATA_URL = './public/data/cars.json';
-const DEVICES_DATA_URL = './public/data/devices.json';
+const CARS_DATA_URL = './data/cars.json';
+const DEVICES_DATA_URL = './data/devices.json';
 const TABLE_COLUMNS = ['RegNumber', 'Device', 'VIN', 'Model', 'InventoryNumber', 'ReleaseDate'];
+const TABLE_CONTAINER = '#tableContainer'
+const FILTER_CONTAINER = '#inputContainer'
 
 async function initializeTable() {
-    try {
-        const [carsData, devicesData] = await Promise.all([
-            fetchJson(CARS_DATA_URL),
-            fetchJson(DEVICES_DATA_URL)
-        ]);
+	try {
+		// Загрузка и слияние данных
+		const [
+			carsData,
+			devicesData
+		] = await Promise.all([
+			fetchJson(CARS_DATA_URL),
+			fetchJson(DEVICES_DATA_URL)
+		]);
+		const mergedData = mergeData(
+			carsData,
+			devicesData,
+			'DeviceId',
+			'Id',
+			{Device: 'Esn'}
+		);
 
-        // Используем вашу функцию mergeData для слияния данных
-        const mergedData = mergeData(carsData, devicesData, 'DeviceId', 'Id', {Device: 'Esn'});
+		// Инициализация таблицы
+		const tableContainer = document.querySelector(TABLE_CONTAINER);
+		const table = new Table(tableContainer);
+		TABLE_COLUMNS.forEach(column => {
+			table.addColumn({title: column});
+		});
+		table.setData(mergedData);
 
-        const tableContainer = document.querySelector('#tableContainer');
-        const table = new Table(tableContainer);
+		// Инициализация инпута для фильтрации
+		const inputContainer = document.querySelector(FILTER_CONTAINER);
+		const input = new Input(inputContainer);
 
-        // Добавляем столбцы в таблицу, используя TABLE_COLUMNS
-        TABLE_COLUMNS.forEach(column => {
-            table.addColumn({ title: column, field: column.toLowerCase() }); // Преобразование для примера, адаптируйте под вашу структуру данных
-        });
+		// Добавление плагинов к таблице
+		table.addPlugin(new SortableTablePlugin(table, TABLE_COLUMNS[0]));
+		table.addPlugin(new FilterableTablePlugin(table, input.element, input.eventName));
 
-        table.setData(mergedData);
-        
-        table.render();
 
-		table.addPlugin(new Sortable());
-		table.addPlugin(new Filterable('#filterInput'));
-    } catch (error) {
-        console.error('Failed to initialize the table:', error);
-    }
+	}
+	catch (error) {
+		console.error('Failed to initialize the table:', error);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', initializeTable);
